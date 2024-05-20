f61668d9d8145b3d7e933ee6c8797665f2bf191f
Sweetgum3897 <97688509+Sweetgum3897@users.noreply.github.com>
2024-05-20 13:21:14 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require("./node_modules/@nomicfoundation/hardhat-toolbox");
 require('solidity-coverage');
 
@@ -10,28 +10,20 @@ require("./node_modules/@nomiclabs/hardhat-ethers");
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
-=======
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
 require('solidity-coverage');
 const { ALCHEMY_ID } = require("@wagmi/core/dist/declarations/src/constants");
-/**
-* @type import('hardhat/config').HardhatUserConfig
-*/
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   defaultNetwork: "goerli",
    networks: {
       goerli: {
          url: process.env.API_URL,
-<<<<<<< HEAD
+
          defaultAlchemyApiKey: process.env.ALCHEMY_ID,
-=======
          defaultAlchemyApiKey: ALCHEMY_ID,
 
          accounts: [process.env.PRIVATE_KEY_DEPLOY]
@@ -41,7 +33,6 @@ module.exports = {
          apiKey: process.env.ETHERSCAN_API,
     },
 };
-<<<<<<< HEAD
-=======
+
 
 
